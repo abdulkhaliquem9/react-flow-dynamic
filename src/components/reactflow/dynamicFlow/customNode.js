b69e3f3e464022351f3727954093bf7e293008aa
abdulkhqliquem9@gmail.com <abdulkhaliquem9@gmail.com>
2024-01-07 01:49:53 +0530

    moved parsers to a file and moved customNode styles to css

--- src/components/reactflow/dynamicFlow/customNode.js
+++ src/components/reactflow/dynamicFlow/customNode.js
@@ -1,4 +1,5 @@
 import { Handle, Position } from 'reactflow';
+import './customNode.css'
 
 export default function CustomNode(props) {
   const { id, data, ...rest } = props
@@ -6,24 +7,24 @@ export default function CustomNode(props) {
     const {label, imgSrc, sourceHandles = [], targetHandles = []} = data
     if(!imgSrc){
       return (
-        <div style={{backgroundColor:'white', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', border: '1px solid black', borderRadius: '3px', width: 'fit-content', minWidth: '50px', height: '50px', textAlign:'center'}}>
+        <div className='node'>
           {
-            sourceHandles.map((sh,i)=> (<Handle type="source" position={Position.Bottom} key={sh} id={sh} style={{visibility: 'hidden', left: '25%', marginBottom: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
+            sourceHandles.map((sh,i)=> (<Handle type="source" position={Position.Bottom} key={sh} id={sh} className='handle' style={{marginBottom: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
           }
           {
-            targetHandles.map((th,i)=> (<Handle type="target" position={Position.Top} key={th} id={th} style={{visibility: 'hidden', left: '25%', marginTop: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
+            targetHandles.map((th,i)=> (<Handle type="target" position={Position.Top} key={th} id={th} className='handle' style={{marginTop: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
           }
           {label}
         </div>
       );
     }else{
       return (
-        <div style={{backgroundColor:'white', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', border: '1px solid black', borderRadius: '3px', width: 'fit-content', minWidth: '50px', height: '50px', textAlign:'center'}}>
+        <div className='node'>
           {
-            sourceHandles.map((sh,i)=> (<Handle type="source" position={Position.Bottom} key={sh} id={sh} style={{visibility: 'hidden', left: '25%', marginBottom: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
+            sourceHandles.map((sh,i)=> (<Handle type="source" position={Position.Bottom} key={sh} id={sh} className='handle' style={{marginBottom: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
           }
           {
-            targetHandles.map((th,i)=> (<Handle type="target" position={Position.Top} key={th} id={th} style={{visibility: 'hidden', left: '25%', marginTop: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
+            targetHandles.map((th,i)=> (<Handle type="target" position={Position.Top} key={th} id={th} className='handle' style={{marginTop: `${(i+1) * 3}px`, marginLeft: `${(i+1) * 3}px`}}/>))
           }
           <span>{label}</span>
           <img width={'40px'} height={'20px'} src={imgSrc} />
---
+++ src/components/reactflow/dynamicFlow/parsers.js
@@ -0,0 +1,137 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const parseNodes = (nodesData = []) => {
+    const nodes = [...nodesData]
+    let x_gap = 60
+    let y_gap  = 100
+    const levels = {}
+    nodes.forEach((node,i) => {
+        if(!levels[node.level]){
+            levels[node.level] = []
+        }
+        levels[node.level].push(node)
+    })
+    const levelKeys = Object.keys(levels)
+    for(let x = levelKeys.length; x >= 1; x--){
+        // console.log('x', x)
+        if(x === 1 || x ==='1'){
+            let start_x = 0
+            levels[x].forEach(((el,i) => {
+                if(!levels[x][i].data){
+                    levels[x][i].data = {}
+                }
+                levels[x][i].data.label = levels[x][i].id
+                start_x += (levels[x][i].child.length === 0 ? 1 : levels[x][i].child.length) * 10;
+                levels[x][i].position = {x: ((levels[levelKeys.length].length / 2) * x_gap) + (i*100)   , y: parseInt(levels[x][i].level) * y_gap}
+            }))
+        }
+        if(levels[x-1]){
+            levels[x-1].forEach((el,eli) => {
+                let start_x = 0;
+                let length = levels[x].length
+                // console.log('x-1',x-1,el.id, levels[x].length,  levels[x-1][eli].child.length)
+                if(levels[x]){
+                    levels[x].forEach((n,ni)=>{
+                        if(!levels[x][ni].data){
+                            levels[x][ni].data = {}
+                        }
+                        if(!levels[x][ni].position){
+                            levels[x][ni].position = {}
+                        }
+                        start_x +=  ((levels[x][ni].child.length === 0 ? 1 : levels[x][ni].child.length)) * x_gap
+                        levels[x][ni].data.label = levels[x][ni].id
+                        levels[x][ni].position.x = start_x;
+                        levels[x][ni].position.y = parseInt(levels[x][ni].level) * y_gap;
+                    })
+                }
+            })
+        }
+    }
+    let nodeData = []
+    Object.keys(levels).forEach(key=> {
+        nodeData = [...nodeData, ...levels[key]]
+    })
+    nodeData = nodeData.map(n => ({...n, type: 'customNode'}))
+    const result = generateEdges(nodeData)
+    // console.log('result', result)
+    return result
+
+}
+
+export const generateEdges = (nodesData = []) => {
+    console.log('generateEdges', nodesData)
+    const nodes = [...nodesData]
+    const imageMap = {
+        'BACKBONE': require('./../icons/SWITCH.png'),
+        'SWITCH': require('./../icons/SWITCH.png'),
+        'SERVER': require('./../icons/SWITCH.png'),
+        'CNM': require('./../icons/SWITCH.png'),
+        'FTE': require('./../icons/SWITCH.png'),
+        'NON-FTE': require('./../icons/SWITCH.png'),
+    }
+    const edges = []
+    const nodeMap = {}
+    nodes.forEach((node,i) => {
+        if(node.id){
+            const data = {...node.data, sourceHandles: [], targetHandles: []}
+            if(imageMap[node.deviceType]){
+                data.imgSrc = imageMap[node.deviceType]
+            }
+            nodeMap[node.id] = {...node,  edges: [], data}
+        }
+    })
+
+    nodes.forEach((node,i) => {
+        if(node.id){
+            if(Array.isArray(node.child)){
+                node.child.forEach((ch,ch_i)=>{
+                    const edgeId = `e-${uuidv4()}`;
+                    const t_handle_Id = `t-${node.id}`
+                    const s_handle_Id = `s-${ch.id}`
+                    // console.log({edgeId, node: nodeMap[node.id], childNode: nodeMap[ch]})
+                    nodeMap[node.id].data.sourceHandles.push(s_handle_Id)
+                    nodeMap[ch.id].data.targetHandles.push(t_handle_Id)
+                    edges.push({
+                        id: `e_s-${node.id}_t-${ch.id}`,
+                        source: node.id,
+                        target: ch.id,
+                        sourceHandle: s_handle_Id,
+                        targetHandle: t_handle_Id,
+                        type: 'customEdge',
+                        data: {conn_color: ch.conn_color || 'red'},
+                    })
+                })
+            }
+        }
+    })    
+    // console.log('...edges', {nodeMap,edges})
+    // return {edges, nodes: Object.values(nodeMap)}
+    /**
+     * el.label is for Aggregated data & el.id is for detailed data
+     */
+    return {edges, nodes: Object.values(nodeMap).map(el => ({...el, data: {...el.data, label: el.label || el.id}}))}
+}
+
+export const parseAggregatedData = (d = {}, l = 1) => {
+    const nodes = []
+    
+    function reccursiveFunction(data = {}, _level = l, _id){
+        const dataObj = {...data}
+        let level = _level; 
+        if (dataObj && Array.isArray(dataObj.deviceIds) && dataObj.deviceIds.length > 0){
+            const {deviceType, childIds = [], child = [], deviceIds, id } = dataObj;
+            const nodeEl = {id,  deviceIds, deviceType, label: `${deviceType} (${deviceIds.length})`, level: `${level}`, child: dataObj.child.map(ch => ({id: ch.id, level: `${level + 1}`, })) }
+            nodes.push(nodeEl)
+            if(Array.isArray(dataObj.child)){
+                dataObj.child.forEach((chObj) => {
+                    reccursiveFunction(chObj, level+1)
+                })
+            }
+            // console.log('......', nodeEl)
+        }
+    }
+
+    reccursiveFunction(d, l)
+    console.log('Nodes--', nodes)
+    return nodes
+}
