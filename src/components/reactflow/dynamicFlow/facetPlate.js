3a4552d9aa4959ab74734c21f9e250c7462385db
abdulkhqliquem9@gmail.com <abdulkhaliquem9@gmail.com>
2024-01-12 02:26:07 +0530

    added facet plate data

---
+++ src/components/reactflow/dynamicFlow/facetPlate.js
@@ -0,0 +1,162 @@
+const data = [
+    [
+      [
+        {
+          device_id: 1,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [3, 4, 6],
+        },
+        {
+          device_id: 3,
+          sub_device_type: 'CF9',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 4,
+          sub_device_type: 'CNM',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 6,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+      ],
+      [
+        {
+          device_id: 2,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [3, 4, 6],
+        },
+        {
+          device_id: 3,
+          sub_device_type: 'CF9',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 4,
+          sub_device_type: 'CNM',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 6,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+      ],
+    ],
+    [
+      [
+        {
+          device_id: 5,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [8, 9],
+        },
+        {
+          device_id: 8,
+          sub_device_type: 'CF9',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 9,
+          sub_device_type: 'CNM',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+      ],
+      [
+        {
+          device_id: 7,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [8, 9],
+        },
+        {
+          device_id: 8,
+          sub_device_type: 'CF9',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 9,
+          sub_device_type: 'CNM',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+      ],
+    ],
+   
+    [
+      [
+        {
+          device_id: 50,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [52, 53],
+        },
+        {
+          device_id: 52,
+          sub_device_type: 'CF9',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 53,
+          sub_device_type: 'CNM',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+      ],
+      [
+        {
+          device_id: 51,
+          sub_device_type: 'SWITCH',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [52, 53],
+        },
+        {
+          device_id: 52,
+          sub_device_type: 'CF9',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+        {
+          device_id: 53,
+          sub_device_type: 'CNM',
+          device_name: 'VC300PM-EHPMX #237',
+          port_name: 'GigabitEthernet1/0/12',
+          child_id_list: [],
+        },
+      ],
+    ],
+  ];
+   
