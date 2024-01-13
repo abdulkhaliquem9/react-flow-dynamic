40cd39260fb4e0bea29bf7c7f37f319536df2940
abdulkhqliquem9@gmail.com <abdulkhaliquem9@gmail.com>
2024-01-14 01:31:07 +0530

    added mouse hover and expand/collapse for facet plates view

--- src/components/reactflow/dynamicFlow/dynamicFlow.js
+++ src/components/reactflow/dynamicFlow/dynamicFlow.js
@@ -1,71 +1,71 @@
-import React, {useEffect, useState} from 'react';
-import ReactFlow, {Controls, MiniMap} from 'reactflow';
+import React, { useEffect, useState } from 'react';
+import ReactFlow, { Controls, MiniMap } from 'reactflow';
 import CustomNode from './customNode';
 import CustomEdge from './customEdge';
 
 import 'reactflow/dist/style.css';
- 
+
 
 const minimapStyle = {
   height: 120,
 };
 export default function DynamicFlow(props) {
-    const {data = {}, enableMouseHover = true, onNodeClick} = props
-    const [chartData, setChartData] = useState(data)
-    const [nodeTypesState, setNodeTypes] = useState({});
-    const [edgeTypesState, setEdgeTypes] = useState({});
-    useEffect(() => {
-        const nodeTypes = {
-            'customNode': (nodeProps) => <CustomNode {...nodeProps} />,
-        };
-        setNodeTypes(nodeTypes);
-        const edgeTypes = {
-            'customEdge': (edgeProps) => <CustomEdge {...edgeProps} />,
-          };
-        setEdgeTypes(edgeTypes)
-    },[])
-
-    useEffect(()=>{
-      setChartData(props.data)
-    }, [props.data])
-
-    const onNodeMouseEnter = (event, node) => {
-      const { id, data } = node;
-      const {nodes, edges} = chartData.data || {}
-      const highlightedEdges = edges.map((edge) => {
-        if (edge.source === id || edge.target === id) {
-          return { ...edge, style: { ...edge.style, strokeWidth: 4 } };
-        } else {
-          return edge;
-        }
-      });
-      // console.log('onMouseOverNode...', { event, node, chartData, highlightedEdges }, '...');
-      setChartData(prev => ({
-        ...prev,
-        edges: highlightedEdges,
-        // [data.chart]: { ...charts[data.chart], edges: highlightedEdges },
-      }) );
+  const { data = {}, enableMouseHover = true, onNodeClick } = props
+  const [chartData, setChartData] = useState(data)
+  const [nodeTypesState, setNodeTypes] = useState({});
+  const [edgeTypesState, setEdgeTypes] = useState({});
+  useEffect(() => {
+    const nodeTypes = {
+      'customNode': (nodeProps) => <CustomNode {...nodeProps} />,
     };
-
-    const onNodeMouseLeave = (event, node) => {
-      const {nodes, edges} = chartData.data || {}
-      const unhighlightedEdges = edges.map((edge) => ({
-        ...edge,
-        style: { ...edge.style, strokeWidth: 2 },
-      }))
-      // console.log('onMouseOut of node...', event, node, '...', unhighlightedEdges);
-      // setEdges(edges.map(edge => ({...edge, style: {...edge.style, strokeWidth: 2}})))
-      setChartData(prev => ({
-        ...prev,
-        edges: unhighlightedEdges,
-        // [data.chart]: { ...charts[data.chart], edges: highlightedEdges },
-      }) );
+    setNodeTypes(nodeTypes);
+    const edgeTypes = {
+      'customEdge': (edgeProps) => <CustomEdge {...edgeProps} />,
     };
+    setEdgeTypes(edgeTypes)
+  }, [])
 
-    // const {nodes, edges}= parseNodes(sampleNodeData.nodes)
-    // const {nodes, edges}= parseNodes(parseAggregatedData(aggregatedJSON))
-    const {nodes, edges} = chartData.data || {}
-    // console.log('...', props, {chartData, nodes, edges})
+  useEffect(() => {
+    setChartData(props.data)
+  }, [props.data])
+
+  const onNodeMouseEnter = (event, node) => {
+    const { id, data } = node;
+    const { nodes, edges } = chartData.data || {}
+    const highlightedEdges = edges.map((edge) => {
+      if (edge.source === id || edge.target === id) {
+        return { ...edge, style: { ...edge.style, strokeWidth: 4 } };
+      } else {
+        return edge;
+      }
+    });
+    // console.log('onMouseOverNode...', { event, node, chartData, highlightedEdges }, '...');
+    setChartData(prev => ({
+      ...prev,
+      edges: highlightedEdges,
+      // [data.chart]: { ...charts[data.chart], edges: highlightedEdges },
+    }));
+  };
+
+  const onNodeMouseLeave = (event, node) => {
+    const { nodes, edges } = chartData.data || {}
+    const unhighlightedEdges = edges.map((edge) => ({
+      ...edge,
+      style: { ...edge.style, strokeWidth: 2 },
+    }))
+    // console.log('onMouseOut of node...', event, node, '...', unhighlightedEdges);
+    // setEdges(edges.map(edge => ({...edge, style: {...edge.style, strokeWidth: 2}})))
+    setChartData(prev => ({
+      ...prev,
+      edges: unhighlightedEdges,
+      // [data.chart]: { ...charts[data.chart], edges: highlightedEdges },
+    }));
+  };
+
+  // const {nodes, edges}= parseNodes(sampleNodeData.nodes)
+  // const {nodes, edges}= parseNodes(parseAggregatedData(aggregatedJSON))
+  const { nodes, edges } = chartData.data || {}
+  // console.log('...', props, {chartData, nodes, edges})
 
   return (
     <div style={{
@@ -73,17 +73,17 @@ export default function DynamicFlow(props) {
       flexDirection: 'column',
       width: '100%',
       height: '100%',
-  }}>
-      <ReactFlow 
-      edgeTypes={edgeTypesState}
-      nodeTypes={nodeTypesState}
-      nodes={nodes} 
-      edges={edges}
-      defaultViewport={{x:0, y: 0, zoom: 0.1}}
-      onNodeMouseEnter={enableMouseHover ? onNodeMouseEnter : null}
-      onNodeMouseLeave={enableMouseHover ? onNodeMouseLeave : null}
-      onNodeClick={onNodeClick}
-      fitView
+    }}>
+      <ReactFlow
+        edgeTypes={edgeTypesState}
+        nodeTypes={nodeTypesState}
+        nodes={nodes}
+        edges={edges}
+        defaultViewport={{ x: 0, y: 0, zoom: 0.1 }}
+        onNodeMouseEnter={enableMouseHover ? onNodeMouseEnter : null}
+        onNodeMouseLeave={enableMouseHover ? onNodeMouseLeave : null}
+        onNodeClick={onNodeClick}
+        fitView
       // onInit = {(reactFlowInstance) => console.log('flow loaded:', reactFlowInstance)}
 
       >
--- src/components/reactflow/dynamicFlow/facePlateView.js
+++ src/components/reactflow/dynamicFlow/facePlateView.js
@@ -7,7 +7,10 @@ import './facePlateView.css'
 function FacePlateView(props) {
     const { data = {}, enableMouseHover = true, onNodeClick } = props
     // const { viewType, data: chartData = [] } = data
+    const [showChart, setShowChart] = useState(true)
+    const [isExpanded, setExpanded] = useState(false)
     const [charstData, setChartsData] = useState(data)
+    const [facePlateData, setFacePlateData] = useState([])
     const [nodeTypesState, setNodeTypes] = useState({});
     const [edgeTypesState, setEdgeTypes] = useState({});
     useEffect(() => {
@@ -22,27 +25,59 @@ function FacePlateView(props) {
     }, [])
 
     useEffect(() => {
-        console.log('DATA CHANGED', props.data)
         setChartsData(props.data)
+        setFacePlateData(props.data.data || [])
     }, [props.data])
+
+    const onNodeMouseEnter = (event, node) => {
+        const { id, data } = node;
+        const { nodes, edges } = facePlateData[0] || {}
+        const highlightedEdges = edges.map((edge) => {
+          if (edge.source === id || edge.target === id) {
+            return { ...edge, style: { ...edge.style, strokeWidth: 4 } };
+          } else {
+            return edge;
+          }
+        });
+        setFacePlateData(prev => ([{nodes, edges: highlightedEdges}]));
+      };
+    
+      const onNodeMouseLeave = (event, node) => {
+        const { nodes, edges } = facePlateData[0] || {}
+        const unhighlightedEdges = edges.map((edge) => ({
+          ...edge,
+          style: { ...edge.style, strokeWidth: 2 },
+        }))
+        setFacePlateData(prev => ([{nodes, edges: unhighlightedEdges}]));
+      };
+
+    const onExpand = (index) => {
+        setFacePlateData([charstData.data[index]])
+        setExpanded(true)
+        setShowChart(false)
+        setTimeout(() => {
+            setShowChart(true)
+        }, 100);
+    }
+    const onCollapse = () => {
+        setFacePlateData(charstData.data)
+        setExpanded(false)
+        setShowChart(false)
+        setTimeout(() => {
+            setShowChart(true)
+        }, 100);
+    }
 
 
     console.log('FacePlateView', charstData)
-    if (charstData && charstData.data && Array.isArray(charstData.data)) {
+    if (showChart && charstData && charstData.data && Array.isArray(charstData.data)) {
         return (
             
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        width: '100%',
-                        height: '100%',
-                    }}
-                >
-                    <div className={'charts-container'}>
+                <div className='face-plate-container'>
+                    <div className='charts-container'>
                     {
-                        charstData.data.map(({ nodes, edges }, i) => {
-                            return <div className={'chart-container'} key={i}>
+                        facePlateData.map(({ nodes, edges }, i) => {
+                            return <div className={!isExpanded ? 'chart-container' : 'chart-container expanded'} key={i}>
                                 <ReactFlow
                                 edgeTypes={edgeTypesState}
                                 nodeTypes={nodeTypesState}
@@ -52,14 +87,21 @@ function FacePlateView(props) {
                                 fitView
                                 attributionPosition="top-right"
                                 defaultViewport={{ x: 0, y: 0, zoom: 0.1 }}
-                                panOnDrag={false}
+                                panOnDrag={isExpanded}
+                                onNodeMouseEnter={isExpanded ? onNodeMouseEnter : undefined}
+                                onNodeMouseLeave={isExpanded ? onNodeMouseLeave : undefined}
                                 // onInit = {(reactFlowInstance) => console.log('flow loaded:', reactFlowInstance)}
-                            // onNodeMouseEnter={enableMouseHover ? onNodeMouseEnter : null}
-                            // onNodeMouseLeave={enableMouseHover ? onNodeMouseLeave : null}
                             // onNodeClick={onNodeClick}
                             >
                                 {/* <MiniMap style={minimapStyle} zoomable pannable /> */}
                             </ReactFlow>
+
+                            {
+                                !isExpanded && <button className={"expand-button"} onClick={()=>onExpand(i)}> Expand </button>
+                            }
+                            {
+                                isExpanded && <button className={"collapse-button"} onClick={onCollapse}> Collapse </button>
+                            }
                             </div>
                         })
                     }
